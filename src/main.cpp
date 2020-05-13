167000182db7c29254d9b063f2e9feef40d4fa37
BabyYod4 <razzlenator@protonmail.com>
2020-05-14 00:51:56 +0200

    Initalization of github repo

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Error Could not initalized GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err){
+      std::cout << "Error Could not initalized GLEW" << std::endl;
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        std::cout << "I Finally have normal output!" << std::endl;
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+
